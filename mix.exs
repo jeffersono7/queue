09a9ffaf336b2afa13f075d1cabacd874de376f2
Jefferson Farias <jefferson.farias7@gmail.com>
2021-12-24 17:13:42 -0300

    publish package config

--- mix.exs
+++ mix.exs
@@ -35,8 +35,7 @@ defmodule Queue.MixProject do
 
   defp package do
     [
-      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
-                license* CHANGELOG* changelog* src),
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
       licenses: ["Apache-2.0"],
       links: %{
         "GitHub" => "https://github.com/jeffersono7/queue"
